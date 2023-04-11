93b169e5c31ca6f5c0d11c64275b325a3705a912
maguirre42 <121882599+maguirre42@users.noreply.github.com>
2023-04-11 13:37:26 +0200

    Initial commit

---
+++ src/main/java/com/eoi/SpringbootDesdeInicioFacturacion/SpringbootDesdeInicioFacturacionApplication.java
@@ -0,0 +1,13 @@
+package com.eoi.SpringbootDesdeInicioFacturacion;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootDesdeInicioFacturacionApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringbootDesdeInicioFacturacionApplication.class, args);
+	}
+
+}
